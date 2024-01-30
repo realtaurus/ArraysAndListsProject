6c38adc7c06262228fed76cea1b14cc9b0b018fe
TaurusA <realtaurus97@gmail.com>
2024-01-30 02:24:25 -0600

    My arrays and list project.

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,44 +1,39 @@
 ﻿using System;
+using System.Collections.Generic; //chat gbt said to add this i dont know why.
 
 namespace ArraysAndLists
 {
     class Program
     {
         static void Main(string[] args)
+        
         {
-            //TODO:
-
-            // Create an int array and populate it with numbers 1-10
+        var myNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             
-
-            /* Create a list of type int
-             * Name the list "evens"
-             */
-
+        var evens = new List<int>();
             
-            /* Create another list of type int
-             * Name the list "odds"
-             */
-            
+        var odds = new List<int>();
 
-             /* Using either a foreach or for loop,
-             * iterate through the array you populated with 10 numbers.
-             * Inside the scope of the loop,
-             * check to see if each number in the array is even or odd.
-             * If the number is even, add it to the evens list.
-             * If the number is odd, add it to the odds list.
-             */
+        foreach (int num in myNumbers)  //these look different than in class but chat gbt said to do it this way.
+        {
+        if (num % 2 == 0)
+        {
+        evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+            }
 
-
-
-            /* Using a foreach loop,
-             * display the numbers in your "evens" list
-             */
-            
-
-            /* Using a for loop,
-             * display the numbers in your "odds" list
-             */
-        }
+            foreach (int num in evens)
+            {
+                Console.WriteLine($"{num} are my evens.");
+            }
+            foreach (int num in odds)
+            {
+                Console.WriteLine($"{num} are my odds."); 
+            }
+         }
     }
 }
